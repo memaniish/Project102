d04d58650b30b464501e2bcc33e22f0822263d1c
Manish <krmanish2197@gmail.com>
2022-01-12 00:32:44 +0530

    commit message

---
+++ TestHelloProject102/TestHelloProject102/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestHelloProject102
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("I'M BATMAN");
+            Console.ReadLine();
+            Fibonnaci fibonnaci = new Fibonnaci();
+            fibonnaci.FindFibonnaci();
+            Console.ReadLine();
+        }
+    }
+}
